0e54267f66cae778dd696de5ca18c40a597fedd7
scamee <zero.monnhann@gmail.com>
2022-03-14 17:46:52 +0900

    tagmodal作成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Facades\Calendar;
 use app\Services\CalendarService;
 use Illuminate\Support\ServiceProvider;
+use App\Models\Tag;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,15 +30,22 @@ class AppServiceProvider extends ServiceProvider
         view()->composer(
             '*',
             function ($view) {
+
                 $user = \Auth::user();
 
-                $view->with('user', $user)->with(
+                $tags = Tag::where('user_id', $user['id'])->get();
+
+                $set_day1 = $tags[0]['set_day'];
+                $set_day2 = $tags[1]['set_day'];
+
+                $view->with('user', $user)->with('tags', $tags)->with(
                     [
                         'weeks'         => Calendar::getWeeks(),
                         'month'         => Calendar::getMonth(),
                         'prev'          => Calendar::getPrev(),
                         'next'          => Calendar::getNext(),
-                        'diff'          => Calendar::diffDay()
+                        'diff1'          => Calendar::diffDay1($set_day1),
+                        'diff2'          => Calendar::diffDay2($set_day2)
                     ]
                 );
             }
